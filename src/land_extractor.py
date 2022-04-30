e51ac165d1a9b7ea91615317db1b78f84978dcb1
pplomith <plomitallopaolo@gmail.com>
2022-04-30 13:14:25 +0200

    Add landmark extraction function

---
+++ src/land_extractor.py
@@ -0,0 +1,29 @@
+import os
+import csv
+import cv2
+import mediap_util as mpu
+
+def process_landmarks(dir,img):
+    filename = 'frames_csv/'+str(img)
+    filename = filename.replace('.png', '.csv')
+
+    # creazione file csv per la singola immagine
+    f = open(filename, 'w')
+    # creazione del writer per scrivere le righe nel csv
+    writer = csv.writer(f)
+    # colonne csv
+    header = ['x','y','z']
+    writer.writerow(header)
+
+    # lettura immagine ed estrazione dei landmark
+    image = cv2.imread(os.path.join(dir, img))
+    num_faces, keypoints = mpu.TESS(image)
+
+    # struttura dati che contiene i landmark divisi per righe (468x3)
+    data = []
+    for row in keypoints:
+        data.append(row)
+    # scrittura di tutte le righe nel csv
+    writer.writerows(data)
+
+    f.close()
