51cc921e0b3a249e1dc46842b5535224927b7589
pplomith <plomitallopaolo@gmail.com>
2022-04-30 13:13:20 +0200

    Update main

--- src/main.py
+++ src/main.py
@@ -1,8 +1,6 @@
 import os
-import mediap_util as mpu
-import cv2
 import Helper as hp
-from multiprocessing import Pool, cpu_count
+import land_extractor as lext
 
 # path del dataset CK+
 path_dataset = hp.getFromEnv('DatasetPath')
@@ -18,30 +16,11 @@ for root, subdirs, files in os.walk(path_dataset):
             for dir in os.listdir(path_sdir):
                 # escludo .DS_STORE e le cartelle nascoste
                 if (not dir.startswith(".")):
-                    # salvataggio all'interno della struttura dati
-                    Dataset_folders.append(os.path.join(path_sdir,dir))
-
-# sort dei path di tutte le cartelle
-Dataset_folders = sorted(Dataset_folders)
-print(Dataset_folders)
-
-def process_landmarks(img):
-    image = cv2.imread(img)
-    num_faces, keypoints = mpu.TESS(image)
-    f = open("Landmark.txt", "a")
-    f.write("\nImage: {}, \nNumber of faces: {}, \nKeyPoints: {}".format(img, num_faces, keypoints))
-    # print("\nImage: {}, \nNumber of faces: {}, \nKeyPoints: {}".format(path_image, num_faces, keypoints))
-    f.close()
+                        # salvataggio all'interno della struttura dati
+                        Dataset_folders.append(os.path.join(path_sdir,dir))
 
 # loop nelle diverse cartelle per andare ad ottenere le singole immagini dei soggetti ed estrarre i landmark
-for dir in Dataset_folders:
+for dir in sorted(Dataset_folders):
     for img in os.listdir(dir):
-        path_image = os.path.join(dir, img)
-        process_landmarks(path_image)
-
-    # num_workers = 2
-    # assert num_workers > 1, "Need more than 1 worker"
-    # pool = Pool(processes=num_workers, maxtasksperchild=1)
-    # pool.map_async(process_landmarks, [os.path.join(dir,img) for img in os.listdir(dir)])
-
+        lext.process_landmarks(dir,img)
 
