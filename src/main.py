bcf1f3ec789238708e4b89aabe0dc157721855d6
pplomith <plomitallopaolo@gmail.com>
2022-05-02 16:32:32 +0200

    Update main

--- src/main.py
+++ src/main.py
@@ -1,26 +1,44 @@
 import os
 import Helper as hp
 import land_extractor as lext
-
+import utils
+import cv2
+from matplotlib import pyplot as plt
 # path del dataset CK+
 path_dataset = hp.getFromEnv('DatasetPath')
+# path label
+path_label = hp.getFromEnv('LabelPath')
 
-# struttura dati che contiene i path delle singole directory di ogni singolo soggetto
-Dataset_folders = []
-for root, subdirs, files in os.walk(path_dataset):
-    for d in subdirs:
-        # considero solo le sottocartelle dei singoli soggetti (iniziano con S)
-        if (os.path.basename(d).startswith("S")):
-            # path fino alle sottocartelle singole (001, 002, 003, etc.)
-            path_sdir = os.path.join(path_dataset, d)
-            for dir in os.listdir(path_sdir):
-                # escludo .DS_STORE e le cartelle nascoste
-                if (not dir.startswith(".")):
-                        # salvataggio all'interno della struttura dati
-                        Dataset_folders.append(os.path.join(path_sdir,dir))
+def processLandmarks(Dataset_folders):
+    # loop nelle diverse cartelle per andare ad ottenere le singole immagini dei soggetti ed estrarre i landmark
+    for dir in sorted(Dataset_folders):
+        for img in os.listdir(dir):
+            lext.process_landmarks(dir,img)
 
-# loop nelle diverse cartelle per andare ad ottenere le singole immagini dei soggetti ed estrarre i landmark
-for dir in sorted(Dataset_folders):
-    for img in os.listdir(dir):
-        lext.process_landmarks(dir,img)
+Dataset_folders = utils.getDirectories(path_dataset)
+# processLandmarks(Dataset_folders)
+Emotion_folders = utils.getDirectories(path_label)
+# print(Emotion_folders)
 
+missing_label = []
+for dir in sorted(Emotion_folders):
+    if len(os.listdir(dir)) == 0:
+        missing_label.append(dir)
+
+f = plt.figure()
+for dir in Dataset_folders:
+    for missing in missing_label:
+        if dir.find(missing[83:]) != -1:
+            print(missing)
+            all_imges = sorted(os.listdir(dir))
+            image = cv2.imread(os.path.join(dir,all_imges[len(os.listdir(dir))-1]))
+            plt.imshow(image)
+            plt.show()
+            while(True):
+                val = input("Enter your value: ")
+                if val != "":
+                    print(val+".0000000e+00")
+                    f = open(os.path.join(missing, all_imges[len(os.listdir(dir)) - 1].replace(".png","_emotion.txt")), 'w')
+                    f.write(val+".0000000e+00")
+                    f.close()
+                    break
