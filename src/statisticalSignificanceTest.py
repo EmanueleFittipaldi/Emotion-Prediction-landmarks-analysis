3e749dde8a7f0bb3a0cbe7fc89074839e10b3cf5
Emanuele Fittipaldi <e.fittipaldi@studenti.unisa.it>
2022-05-08 12:49:20 +0200

    Update statisticalSignificanceTest.py. Improved readability, routines modularized through functions, redudant or excessive comments removed

--- src/statisticalSignificanceTest.py
+++ src/statisticalSignificanceTest.py
@@ -4,102 +4,109 @@ from scipy import stats
 from statistics import mode
 import numpy as np
 
-def pvaluePlotter(pvalueHistory,splits):
+
+def pvaluePlotter(Pvalue_history,splits):
+    """
+    This Function takes a list of pvalues and a list of splits where these pvalues where calculated and
+    creates a plot showing how pvalue changed according to the several splits.
+        - **Returns**: nothing
+
+        - Parameter **Pvalue_history**: a list of pvalues
+
+        - Parameter **splits**: a list of the split points where the pvalues where valuated
+
+        - **Precondition**: Pvalue_history and splits are lists of numbers
+    """
     x = np.array(splits)
     y = np.array(Pvalue_history)
     plt.ylabel("pvalue")
     plt.plot(x, y, color = "red", marker = "o", label = "pvalue")
     plt.legend()
     plt.show()
-
-
+def histogramPlotter(data):
+    plt.hist(data)
+    plt.show()
 def normalTest(values):
+    """
+    This function takes a list of numerical values and conduct a normal test over these values returning a p2 value.
+    If this value is less than alpha, we conclude that the values were not drawn from a normal distribuition.
+
+    - **Returns**: 1 if we can reject the null hypotesis, meaning that the values were not drawn from a normal distribuition,otherwise we return 0, meaning that the values were drawn from a normal distribuition.
+    - **Value return** has type int.
+    - Parameter **values**: the list of numerical values on which we want to conduct the normal test
+    - **Precondition**: values is a list of numerical data
+
+    """
     stat, p2 = stats.normaltest(values)
     alpha = 1e-3
     # print("p = {}".format(p2))
     if p2 < alpha:
+        # the values were not drawn from a normal distribuition
         # print("the null hp can be rejected")
         return 1
     else:
+        # the values were drawn from a normal distribuiton
         # print("the null hp cannot be rejected")
         return 0
+def normalizeData(data):
+    return np.log(data)
+def pvalueTest(vec1,vec2,alphaValue):
+    res = stats.ttest_ind(vec1,vec2).pvalue
+    if res <= alphaValue:
+        return True,res
+    else:
+        return False,res
 
 
 # Carico il csv contenente le distanze di una sequenza video da analizzare
 videoSequence = pd.read_csv("Local_Distances/S506_001_LD_euclidean.csv", header=None)
 
-# array contenente la variazione del pvalue in base al frame in cui mi trovo.
-# es. tra il frame 2-3 il pvalue vale 0.80, tra il frame 3-4 il pvalue vale 0.30, etc ...
-# l'ipotesi è che tra tutti questi pvalue ci sia un picco netto che dimostra una variazione significativa
-# in termini di distanza per il landmark che stiamo considerando (in questo esempio sto misurando la significatività
-# soltanto del landmark 0).
-#
-# Come calcolare il pvalue:
-# Per poter calcolare il pvalue devo avere due insiemi di dati che io considero differenti. Nel nostro caso
-# l'ipotesi è che i valori delle distanze di un landmark, fino ad un certo punto appartengono ad una microespressione
-# ma da un certo punto in poi (che vogliamo scoprire) appartengono alla distribuzione di dati di una macroespressione.
-# devo quindi dividere il dataframe delle distanze in due fette orizzontalmente, dove la prima fetta sono i valori
-# che io dico appartenere alla microespressione mentre la seconda fetta contiene i valori che io dico appartenere
-# alla macroespressione. Dato che io non conosco effettivamente dov'è questo punto, ovvero dove la microespressione
-# diventa macroespressione, devo provare a "tagliare" il dataset provando diversi split. Per es. se la mia videosequenza
-# è composta da 10 frame, allora provo prima a dire che: "i primi 2 frame sono microespressione e i successivi 8" sono
-# macroespressione, calcolo il pvalue e lo memorizzo in Pvalue_history. Faccio questa cosa fino a quando non splitto
-# il dataframe in len-2 frame di microespressione e 2 frame di macroespressione. L'ipotesi è che quando il pvalue raggiunge
-# un picco per la prima volta allora siamo passati da micro a macro-espressione.
-Pvalue_history = [] # lista dei valori che il pvalue ha assunto per ciascun landmark e per ciascun split
-splitsChangeOccurred = [] # lista dei frame in cui è avvenuta una variazione significativa di un landmark
-landmarksInvolved = [] # lista dei landmark che hanno subito una variazione di posizione significativa all'interno della videosequenza
-
-splits = list(range(2,len(videoSequence)-1))
-# Regolare questo valore tra 0.05 e 0.01 per incrementare oppure decrementare la soglia per la quale decretiamo
-# la variazione di posizione di un landmark come significativa oppure no. Mettendo una soglia molto bassa come 0.01
-# otteniamo i landmark che più rappresentativamente hanno oltrepassato la soglia alpha.
-
-# Grafici per visualizzare l'andamento dei dati dopo averli normalizzati
-# data = videoSequence.iloc[1:, :1]
-# plt.hist(data)
-# plt.show()
-# plt.hist(np.log(data))
-# plt.show()
-
-print(videoSequence)
-# Per poter applicare il test di significatività su ciascun landmark abbiamo bisogno che le distanze registrate seguino
-# una distribuzione normale (gaussiana) altrimenti non è possibile applicarlo.
-# senza la normalizzazione abbiamo riscontrato in media 150 landmark che non seguivano una distribuzione normale.
-# dopo la trasformazione abbiamo ridotto il numero a circa 20 landmark, non è possibile migliorarlo
-# poiché le distanze registrate non sono di un numero sufficientemente ampie
-# plt.hist(videoSequence.iloc[1:, 3:4])
-# plt.show()
+# Pvalue_history: lista dei valori che il pvalue ha assunto per ciascun landmark e per ciascun split
+# splitsChangeOccurred: lista dei frame in cui è avvenuta una variazione significativa di un landmark
+# landmarksInvolved: lista dei landmark che hanno subito una variazione di posizione significativa all'interno della
+# videosequenza
+Pvalue_history = []
+splitsChangeOccurred = []
+landmarksInvolved = []
 outlier = []
+
+
+# lista di split per il quale voglio calcolare il pvalue
+splits = list(range(2,len(videoSequence)-1))
+
+
+# Visualizzazione dell'istogramma delle distanze di un solo landmark prima e dopo la normalizzazione
+# data = videoSequence.iloc[1:, :1]
+# histogramPlotter(data)
+# normlized = normalizeData(data)
+# histogramPlotter(normalized)
+
+
 for i in range(0,len(videoSequence.columns)-2):
     data = videoSequence.iloc[1:, i:i+1]
     videoSequence.iloc[1:, i:i+1] = abs(np.log(data))
     if normalTest(videoSequence.iloc[1:, i:i+1]):
         outlier.append(i)
 print("Numero di landmark non normalizzati: {}, landmark: {}".format(len(outlier), outlier))
-# plt.hist(videoSequence.iloc[1:, 3:4])
-# plt.show()
-
-print(videoSequence)
-
 
 alpha = 0.01
-
 for split in splits:
     df_1 = videoSequence.iloc[:split, :]
     df_2 = videoSequence.iloc[split:, :]
     for i in range(0,467):
         landmark = i
-        res = stats.ttest_ind(df_1[landmark],df_2[landmark]).pvalue
-        if res <= alpha:
+        significant = pvalueTest(df_1[landmark],df_2[landmark],alpha)
+        if significant[0]:
             # print("STATISTICAL SIGNIFICANCE DETECTED")
             # print("pvalue{}, split{}, landmark{}".format(res,split,landmark))
             splitsChangeOccurred.append(split)
             landmarksInvolved.append(landmark)
-            Pvalue_history.append(res)
+            Pvalue_history.append(significant[1])
 
+# STATS PRINTING
 print("\nThis is the frame where the major number of landmarks had a significant variation in the distance")
 print(mode(splitsChangeOccurred))
+
 print("This is the list of the landmark involved in this emotion")
 landmarksInvolved = set(landmarksInvolved)
 landmarksInvolved = sorted(list(landmarksInvolved))
