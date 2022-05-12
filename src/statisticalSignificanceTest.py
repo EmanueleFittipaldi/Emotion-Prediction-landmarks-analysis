f341dc1f53c5d3fea6f0cb8ea5342f9915b86cea
Emanuele Fittipaldi <e.fittipaldi@studenti.unisa.it>
2022-05-12 17:31:00 +0200

    Update statisticalSignificanceTest.py

--- src/statisticalSignificanceTest.py
+++ src/statisticalSignificanceTest.py
@@ -4,23 +4,16 @@ from utils import *
 from mpl_toolkits.mplot3d import axes3d
 import matplotlib.pyplot as plt
 import operator
-
-# Carico il csv contenente le distanze di una sequenza video da analizzare
-videoSequence = pd.read_csv("Local_Distances/S136_001_LD_euclidean.csv", header=None)
-
 # Pvalue_history: lista dei valori che il pvalue ha assunto per ciascun landmark e per ciascun split
 # splitsChangeOccurred: lista dei frame in cui è avvenuta una variazione significativa di un landmark
 # landmarksInvolved: lista dei landmark che hanno subito una variazione di posizione significativa all'interno della
 # videosequenza
-Pvalue_history = []
-splitsChangeOccurred = []
-landmarksInvolved = []
-outlier = []
 
 
-# lista di split per il quale voglio calcolare il pvalue
-splits = list(range(2,len(videoSequence)-1))
 
+
+Local_Distances_euclidean = ["Local_Distances/"+x for x in os.listdir("Local_Distances/") if "euclidean.csv" in x]
+Local_Distances_manhattan = ["Local_Distances/"+x for x in os.listdir("Local_Distances/") if "manhattan.csv" in x]
 
 # Visualizzazione dell'istogramma delle distanze di un solo landmark prima e dopo la normalizzazione
 # data = videoSequence.iloc[1:, :1]
@@ -39,22 +32,28 @@ splits = list(range(2,len(videoSequence)-1))
 #     if normalTest(videoSequence.iloc[1:, i:i+1]):
 #         outlier.append(i)
 # print("Numero di landmark non normalizzati: {}, landmark: {}".format(len(outlier), outlier))
-alpha = 0.01
+alpha = 0.05
+for path in Local_Distances_euclidean:
+    videoSequence = pd.read_csv(path, header=None)
+    splits = list(range(2, len(videoSequence) - 1))
+    LandmarksPerSplit = {}
+    for split in splits:
+        landmarkSignificativi = []
+        df_1 = videoSequence.iloc[:split, :]
+        df_2 = videoSequence.iloc[split:, :]
+        for i in range(0,467):
+            landmark = i
+            significant = pvalueTest(df_1[landmark],df_2[landmark],alpha)
+            if significant[0]:
+                landmarkSignificativi.append(landmark)
+        LandmarksPerSplit[split] = landmarkSignificativi
+    maxValue = 0
 
-for split in splits:
-    df_1 = videoSequence.iloc[:split, :]
-    df_2 = videoSequence.iloc[split:, :]
-    for i in range(0,467):
-        landmark = i
-        significant = pvalueTest(df_1[landmark],df_2[landmark],alpha)
-        if significant[0]:
-            # print("STATISTICAL SIGNIFICANCE DETECTED")
-            # print("pvalue{}, split{}, landmark{}".format(res,split,landmark))
-            splitsChangeOccurred.append(split)
-            landmarksInvolved.append(landmark)
-            Pvalue_history.append(significant[1])
-    print("#:{}, split:{}".format(len(landmarksInvolved),split))
-    landmarksInvolved = []
+    for i in range(2,len(LandmarksPerSplit)):
+
+
+    # print("Subject:{} #:{}, split:{}, landmarks:{}".format(path[16:24],len(landmarkSignificativi),split,landmarkSignificativi))
+    # print("-----------------------------------------------")
 
 # STATS PRINTING
 # print("\nThis is the frame where the major number of landmarks had a significant variation in the distance")
